088de1e181d99f0b56003347add09ed9175e2f25
alexandre zanella <alexandre.zanella@alcidesmaya.edu.br>
2020-04-23 18:24:43 -0300

    Initial commit

---
+++ app/src/main/java/info/webinformatica/basic/z1/MainActivity.java
@@ -0,0 +1,14 @@
+package info.webinformatica.basic.z1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
